91751fe5af1caf516bd5c3887056f0d29ce33460
Ogawa Kei <keisiotane@gmail.com>
2023-05-08 18:32:29 -0300

    create aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+/* var pizza = 8
+while(pizza > 0){
+        pizza--
+        console.log(`Comi um pedaço, agora sombraram ${pizza} pedaços da pizza!`)
+}
+ */
+
+var pizza = 8
+do{
+        pizza--
+        console.log(`Comi um pedaço, agora sombraram ${pizza} pedaços da pizza!`)
+} while(pizza > 0)
---
+++ aula13/node_fb75b7c6a5b18.tmp.js
@@ -0,0 +1,6 @@
+
+var pizza = 8
+while(pizza > 0){
+        pizza - 1;
+        console.log(`Comi um pedaço, agora sombraram ${pizza} pedaços da pizza!`)
+}
