27f10ef0c3d9c8fe0f48340591e2a08fbb2a73df
Ogawa Kei <keisiotane@gmail.com>
2023-05-22 14:32:28 -0300

    aula14

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -11,6 +11,15 @@ do{
         console.log(`Comi um pedaço, agora sombraram ${pizza} pedaços da pizza!`)
 } while(pizza > 0) */
 
-for (var c = 1; c <= 5; c++){
+/* for (var c = 1; c <= 5; c++){
         console.log(c)
-}
+} */
+
+/* var c = 1
+do{
+        console.log(c)
+        c++
+}while (c <= 5)
+ */
+
+//
