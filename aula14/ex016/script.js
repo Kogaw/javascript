df14bfbfd142b647790077f85a80530772f9dc5b
Ogawa Kei <keisiotane@gmail.com>
2023-05-29 18:31:32 -0300

    Update script.js

---
+++ aula14/ex016/script.js
@@ -0,0 +1,9 @@
+var inicio = document.getElementById('txtini')
+var fim = document.getElementById('txtfim')
+var pass = document.getElementById('txtpas')
+var n = 4
+
+function contar(){
+    for(n = inicio; n < fim; n++)
+    res.innerHTML = `${n}`
+}
