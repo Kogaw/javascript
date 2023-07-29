09e1cb39a7ae9535c45ea35385a055fa038666bb
Ogawa Kei <keisiotane@gmail.com>
2023-07-29 17:33:43 -0300

    update

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -2,8 +2,11 @@ function contar(){
     let inicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    for(n = inicio; n < fim; n++){
-    res.innerHTML = `${n}`
-    }
+    let res = document.getElementById('res')
 
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }else{
+        res.innerHTML = 'Contando:'
+    }
 }
