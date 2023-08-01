69ae3d87b03c82fc98929595d17d0deb7f69397d
Ogawa Kei <keisiotane@gmail.com>
2023-08-01 20:24:17 -0300

    Update script.js

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -5,23 +5,28 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        //window.alert('[ERRO] Faltam dados!')
     }else{
         res.innerHTML = 'Contando:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        if(i < f){     
-               for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c} \u{1F449}`
+        if(p <= 0){
+            window.alert('[ERRO] Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-        } else{
-            for(let c = 1; c >=f; c-=p){
+
+        if(i < f){    
+
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+
+            for(let c = i; c >=f; c -= p){
                 res.innerHTML += `${c} \u{1F449}`
             }
         }
         res.innerHTML += `\u{1F3C1}`
-
     }
 }
