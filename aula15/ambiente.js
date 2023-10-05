81f41265dc137f47f4ab67e65976b8774ee434b8
Ogawa Kei <keisiotane@gmail.com>
2023-10-05 20:43:15 -0300

    update

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -6,3 +6,13 @@ console.log(`Nosso vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
 
+for(let pos=0; pos<num.length;pos++){
+    console.log(num[pos])
+}
+
+let pos = num.indexOf(4)
+if (pos == -1){
+    console.log('O valor não foi encontrado')
+}else{
+console.log(`O valor 8 está na posição ${pos}`)
+}
