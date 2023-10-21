6d0efa694ac5b0df7e57037440d62ef3d3584d8a
Ogawa Kei <keisiotane@gmail.com>
2023-10-20 22:31:42 -0300

    update

---
+++ aula16/ex019/script.js
@@ -0,0 +1,30 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valore = []
+
+function isNumero(n){
+    if(Number(n) >=1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
